e666052779072fc352d0039b4338af9c9dfc1efe
kpillaai <kpillaai@gmail.com>
2025-05-11 13:49:25 +1000

    api working, displaying movies and images properly. searching also working.

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -5,10 +5,32 @@ function MovieCard({ movie }) {
     alert("clicked");
   }
 
+  function getMonth(monthNumber) {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    return monthNames[monthNumber];
+  }
+
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
       </div>
       <div className="movie-overlay">
         <button className="favourite-btn" onClick={onFavouriteClick}>
@@ -17,7 +39,10 @@ function MovieCard({ movie }) {
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>
+          {getMonth(parseInt(movie.release_date?.split("-")[1]))}{" "}
+          {movie.release_date?.split("-")[0]}
+        </p>
       </div>
     </div>
   );
